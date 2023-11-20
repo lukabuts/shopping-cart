63c8d6f36140a55c355e172977a48733984647b6
lukabuts <butskhrikidzeluka0@gmail.com>
2023-11-21 00:36:43 +0400

    Changed BrowserRouter into HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
 import "./app.css";
